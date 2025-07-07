b5941f5de66d1b34fdede43b54cf6e98b643103e
Paco <kifouetobosmwansilapacochristi@gmail.com>
2025-07-07 23:06:17 +0100

    changes

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -28,16 +28,3 @@ class Program
         Console.WriteLine(myResume._jobs[0]._jobTitle);
     }
 }
-public class Job
-        {
-            public string _jobTitle;
-            public string _company;
-            public int _startYear;
-            public int _endYear;
-
-            public void DisplayJobDetails()
-            {
-                Console.WriteLine($"{_jobTitle} ({_company}) {_startYear} {_endYear}");
-            }
-
-        }
