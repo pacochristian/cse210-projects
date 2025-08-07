a3b2fafe55953f3ac2ae2a8ee0aa62a4082acb62
Paco <kifouetobosmwansilapacochristi@gmail.com>
2025-08-07 17:07:34 +0100

    chnages

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Console.WriteLine("Hello World! This is the EternalQuest Project.");
+
+        List<Shape> shapes = new List<Shape>
+        {
+            new Circle("Red", 5),
+            new Rectangle("Blue", 4, 6),
+            new Square("Green", 3)
+        };
+
+        foreach (var shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.GetColor()}");
+            Console.WriteLine($"Shape Area: {shape.GetArea()}");
+        }
     }
 }
